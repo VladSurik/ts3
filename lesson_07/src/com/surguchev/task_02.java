1a0c6755e21a0bb19935d5c40d9783c9db4b5e36
Vlad Surguchev <surgutchov@gmail.com>
2021-02-24 23:13:10 +0200

    task 02 lucky tickets

---
+++ lesson_07/src/com/surguchev/task_02.java
@@ -0,0 +1,39 @@
+package com.surguchev;
+
+//Найти количество счастливых билетиков на трамвай от 000001 до 999999 (те у которых сумма первых 3 цифр равна сумме последних 3)
+
+public class task_02 {
+
+    public static final int minNumberTickets = 1;
+    public static final int maxNumberTickets = 999999;
+
+    public static void main(String[] args) {
+        int countLuckyTickets = 0;
+
+        for (int i = minNumberTickets; i <= maxNumberTickets; i++) {
+            String strNum = String.valueOf(i);
+            if (strNum.length() > 3) {
+                if (chekSumLeftAndRigth(strNum)) {
+                    countLuckyTickets++;
+                }
+            }
+        }
+        System.out.println("number of lucky tickets: " + countLuckyTickets);
+    }
+
+    private static boolean chekSumLeftAndRigth(String num) {
+        int sumRigth = 0;
+        int sumLeft = 0;
+        int cntRigthNumberForSum = 0;
+        String[] arrayNum = num.split("");
+        for (int j = arrayNum.length - 1; j >= 0; j--) {
+            if (cntRigthNumberForSum > 2) {
+                sumLeft += Integer.parseInt(arrayNum[j]);
+            } else {
+                sumRigth += Integer.parseInt(arrayNum[j]);
+            }
+            cntRigthNumberForSum++;
+        }
+        return sumLeft == sumRigth;
+    }
+}
